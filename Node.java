0aa421f2861464bead4a2ffa58594344d8d68a62
Daniel Bachler <daniel.bachler@comcast.net>
2017-10-13 10:14:32 -0600

    Finished node class, I think

--- Node.java
+++ Node.java
@@ -1,10 +1,10 @@
 public class Node {
     public int value;
     public char label;
-    public int[] neighbors;
+    public Node[] neighbors;
     public boolean visited = false;
 
-    public Node(int value, char label, int[] neighbors) {
+    public Node(int value, char label, Node[] neighbors) {
         this.value = value;
         this.label = label;
         this.neighbors = neighbors;
@@ -12,7 +12,11 @@ public class Node {
 
     //Checks for unvisited neighbors, if none are found then -1 is returned
     public int firstUnvisitedNeighbor() {
-
+        for(int i = 0; i < neighbors.length; i++) {
+            if(!neighbors[i].visited) {
+                return i;
+            }
+        }
         return -1;
     }
 }
